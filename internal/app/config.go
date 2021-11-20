84066b708adcf87a6f287c99dc1c64f72f2cf305
Adrian PK <adrianpk@adrianpk.com>
2021-11-20 15:40:16 +0100

    Support for API configuration loading

--- internal/app/config.go
+++ internal/app/config.go
@@ -2,12 +2,15 @@ package app
 
 import (
 	"flag"
+	"os"
+	"strings"
 )
 
 type (
 	Config struct {
 		Server
 		Logging
+		NASAAPI
 	}
 
 	Server struct {
@@ -17,18 +20,52 @@ type (
 	Logging struct {
 		Level string
 	}
+
+	NASAAPI struct {
+		APIKEYEnvar string
+		APIKEY      string
+	}
 )
 
+const (
+	nasaDefaultAPIKEY = "DEMO_KEY"
+)
+
+// NOTE: A little update eventually would allow all envar names to be passed
+// as parameters instead of the actual values.
 func LoadConfig() *Config {
-	c := Config{}
+	cfg := Config{}
 
 	// Server
-	flag.IntVar(&c.Server.JSONAPIPort, "json-api-port", 8081, "JSON API server port")
+	flag.IntVar(&cfg.Server.JSONAPIPort, "json-api-port", 8081, "JSON API server port")
 
 	// Logging
-	flag.StringVar(&c.Logging.Level, "logging-level", "error", "Logging level")
+	flag.StringVar(&cfg.Logging.Level, "logging-level", "error", "Logging level")
+
+	// NASA API
+	flag.StringVar(&cfg.NASAAPI.APIKEYEnvar, "nasa-api-key-envar", "", "NASA API Key envar")
 
 	flag.Parse()
 
-	return &c
+	cfg.LoadFromEnvar()
+
+	return &cfg
+}
+
+func (cfg *Config) LoadFromEnvar() {
+	cfg.NASAAPI.LoadFromEnvar()
+}
+
+func (na *NASAAPI) LoadFromEnvar() {
+	ak := loadEnvar(na.APIKEYEnvar)
+
+	if "" == strings.TrimSpace(ak) {
+		ak = nasaDefaultAPIKEY
+	}
+
+	na.APIKEY = ak
+}
+
+func loadEnvar(name string) string {
+	return os.Getenv(name)
 }
