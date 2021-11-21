131e97f3204f8363e46972f0c80e0ef6c0f4f99f
Adrian K <adrian@kugo.tech>
2021-11-21 19:57:25 +0100

    Search between dates bare implementation

--- internal/app/config.go
+++ internal/app/config.go
@@ -3,6 +3,7 @@ package app
 import (
 	"flag"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -22,13 +23,17 @@ type (
 	}
 
 	NASAAPI struct {
-		APIKEYEnvar string
-		APIKEY      string
+		APIKEYEnvar      string
+		APIKEY           string
+		MaxRequestsEnvar string
+		MaxRequests      int
+		TimeoutInSecs    int
 	}
 )
 
 const (
-	nasaDefaultAPIKEY = "DEMO_KEY"
+	nasaDefaultAPIKEY      = "DEMO_KEY"
+	nasaDefaultMaxRequests = 5
 )
 
 // NOTE: A little update eventually would allow all envar names to be passed
@@ -44,6 +49,8 @@ func LoadConfig() *Config {
 
 	// NASA API
 	flag.StringVar(&cfg.NASAAPI.APIKEYEnvar, "nasa-api-key-envar", "", "NASA API Key envar")
+	flag.StringVar(&cfg.NASAAPI.MaxRequestsEnvar, "nasa-api-concurrent-requests-envar", "CONCURRENT_REQUESTS", "NASA API max concurrent requests envar")
+	flag.IntVar(&cfg.NASAAPI.TimeoutInSecs, "nasa-api-timeout-in-secs", 5, "NASA API timeout in secs")
 
 	flag.Parse()
 
@@ -64,8 +71,25 @@ func (na *NASAAPI) LoadFromEnvar() {
 	}
 
 	na.APIKEY = ak
+
+	maxReq := toInt(loadEnvar(na.MaxRequestsEnvar))
+	if maxReq < 1 {
+		maxReq = nasaDefaultMaxRequests
+	}
+
+	na.MaxRequests = maxReq
+
 }
 
 func loadEnvar(name string) string {
 	return os.Getenv(name)
+}
+
+func toInt(intString string) int {
+	i, err := strconv.Atoi(intString)
+	if err == nil {
+		return 0
+	}
+
+	return i
 }
