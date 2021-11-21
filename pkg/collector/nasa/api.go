e4100658df14dd3f77d0c215a888a4c5b3f4406e
Adrian K <adrian@kugo.tech>
2021-11-21 22:58:15 +0100

    Getting URLs from NASA API

--- pkg/collector/nasa/api.go
+++ pkg/collector/nasa/api.go
@@ -3,12 +3,25 @@ Package nasa provides a basic implementation of collector.Collector.
 For the sake of simplicity at the moment is implemented within the service libs
 but eventually they could be moved to a custom module in order to facilitate
 their reuse.
+
+Sample JSON response
+{
+	"date":"2020-01-01",
+	"explanation":"Why is Betelgeuse fading?  No one knows.  Betelgeuse, one of the brightest and most recognized stars in the night sky, is only half as bright as it used to be only five months ago.  Such variability is likely just  normal behavior for this famously variable supergiant, but the recent dimming has rekindled discussion on how long it may be before Betelgeuse does go supernova.  Known for its red color, Betelgeuse is one of the few stars to be resolved by modern telescopes, although only barely.  The featured artist's illustration imagines how Betelgeuse might look up close. Betelgeuse is thought to have a complex and tumultuous surface that frequently throws impressive flares.  Were it to replace the Sun (not recommended), its surface would extend out near the orbit of Jupiter, while gas plumes would bubble out past Neptune.  Since Betelgeuse is about 700 light years away, its eventual supernova will not endanger life on Earth even though its brightness may rival that of a full Moon.  Astronomers -- both amateur and professional -- will surely continue to monitor Betelgeuse as this new decade unfolds.    Free Presentation: APOD Editor to show best astronomy images of 2019 -- and the decade -- in NYC on January 3",
+	"hdurl":"https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_2662.jpg",
+	"media_type":"image",
+	"service_version":"v1",
+	"title":"Betelgeuse Imagined",
+	"url":"https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_960.jpg"
+}
 */
 package nasa
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -37,6 +50,19 @@ type (
 )
 
 type (
+	APIResponse struct {
+		Copyright      string `json:"copyright"`
+		Date           string `json:"date"`
+		Explanation    string `json:"explanation"`
+		HDURL          string `json:"hdurl"`
+		MediaType      string `json:"media_type"`
+		ServiceVersion string `json:"service_version"`
+		Title          string `json:"title"`
+		URL            string `json:"url"`
+	}
+)
+
+type (
 	// NOTE: A future update could include retries with exponential back off.
 	httpClient struct {
 		*http.Client
@@ -59,24 +85,46 @@ func NewAPI(opts Options) *API {
 	}
 }
 
-func (api API) GetBetweenDates(from, to time.Time) collector.Result {
-	for dateElement := rangeDates(from, to); ; {
-		date := dateElement()
-		if date.IsZero() {
-			break
+func (api API) GetBetweenDates(from, to time.Time) (cr collector.Result, err error) {
+	aarr := []*APIResponse{}
+
+	func() error {
+		for dateElement := rangeDates(from, to); ; {
+			date := dateElement()
+			if date.IsZero() {
+				return nil
+			}
+
+			//fmt.Println(toDateString(date))
+
+			ar, err := api.getByDate(date)
+			if err != nil {
+				return fmt.Errorf("error retrieving URL: %w", err)
+			}
+
+			aarr = append(aarr, ar)
 		}
+	}()
 
-		//fmt.Println(toDateString(date))
-
-		api.getByDate(date)
+	list := []string{}
+	for _, ar := range aarr {
+		list = append(list, ar.URL)
 	}
 
-	return Result{}
+	cr = &Result{
+		list: collector.URLS{
+			List: list,
+		},
+	}
+
+	fmt.Printf("\nResult:\n%+v\n", cr)
+
+	return cr, nil
 }
 
-func (api API) getByDate(date time.Time) (picURL map[string]string, err error) {
+func (api API) getByDate(date time.Time) (ar *APIResponse, err error) {
 	if date.IsZero() {
-		return picURL, errors.New("Invalid date")
+		return ar, errors.New("Invalid date")
 	}
 
 	dateStr := toDateString(date)
@@ -85,22 +133,23 @@ func (api API) getByDate(date time.Time) (picURL map[string]string, err error) {
 
 	res, err := api.client.get(url)
 	if err != nil {
-		return picURL, fmt.Errorf("Error retrievieng picture URL: %w", err)
+		return ar, fmt.Errorf("Error retrievieng picture URL: %w", err)
 	}
 
 	fmt.Printf("Request:\n%+v\n", res)
-	// NOTE: Sample JSON response
-	// {
-	// 	"date":"2020-01-01",
-	// 	"explanation":"Why is Betelgeuse fading?  No one knows.  Betelgeuse, one of the brightest and most recognized stars in the night sky, is only half as bright as it used to be only five months ago.  Such variability is likely just  normal behavior for this famously variable supergiant, but the recent dimming has rekindled discussion on how long it may be before Betelgeuse does go supernova.  Known for its red color, Betelgeuse is one of the few stars to be resolved by modern telescopes, although only barely.  The featured artist's illustration imagines how Betelgeuse might look up close. Betelgeuse is thought to have a complex and tumultuous surface that frequently throws impressive flares.  Were it to replace the Sun (not recommended), its surface would extend out near the orbit of Jupiter, while gas plumes would bubble out past Neptune.  Since Betelgeuse is about 700 light years away, its eventual supernova will not endanger life on Earth even though its brightness may rival that of a full Moon.  Astronomers -- both amateur and professional -- will surely continue to monitor Betelgeuse as this new decade unfolds.    Free Presentation: APOD Editor to show best astronomy images of 2019 -- and the decade -- in NYC on January 3",
-	// 	"hdurl":"https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_2662.jpg",
-	// 	"media_type":"image",
-	// 	"service_version":"v1",
-	// 	"title":"Betelgeuse Imagined",
-	// 	"url":"https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_960.jpg"
-	// }
 
-	return picURL, nil
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return ar, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK {
+		json.Unmarshal(body, &ar)
+	}
+
+	return ar, nil
 }
 
 func (r Result) GetList() collector.URLS {
